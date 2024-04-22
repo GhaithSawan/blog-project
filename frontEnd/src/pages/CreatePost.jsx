c28255ed9f5f8e1349c1594464b6a471dec2f89f
Ghaith <gaesx1x2@gmail.com>
2024-04-22 07:30:43 -0700

    d

--- frontEnd/src/pages/CreatePost.jsx
+++ frontEnd/src/pages/CreatePost.jsx
@@ -3,27 +3,61 @@ import React from "react";
 const CreatePost = () => {
   return (
     <div className="allpages">
-      <div className="container d-flex align-items-center justify-content-center flex-column p-3">
-        <h3
-          style={{ width: "max-content" }}
+      <div
+        className="container d-flex align-items-center justify-content-center flex-column p-3"
+        style={{ width: "1000px"}}
+      >
+        <h2
+          style={{ width: "max-content", fontWeight: "500" }}
           className=" border-bottom border-2 border-dark "
         >
           Create New Post
-        </h3>
+        </h2>
 
         <form className="container d-flex align-items-center justify-content-center flex-column ">
-          <input type="text" placeholder="Add title to your post" />
-          <select className="select_father">
-            <option disabled value="">
+          <input
+            type="text"
+            placeholder="Add title to your post"
+            style={{
+              borderRadius: "5px",
+              width: "70%",
+              margin: "40px 0 10px 0",
+              padding: "10px",
+              border: "1px #778697 solid",
+            }}
+          />
+          <select
+            className="select_father"
+            style={{
+              borderRadius: "5px",
+              width: "70%",
+              margin: "10px 0 10px 0",
+              padding: "10px",
+              border: "1px #778697 solid",
+            }}
+          >
+            <option disabled value=" ">
               Select A Category
             </option>
-            <option disabled value="">
-              Select A Category
-            </option>
-            <option value="">Coffee</option>
+            <option value="Coffee">Coffee</option>
+            <option value="music">music</option>
           </select>
-          <textarea placeholder="Add Description" rows="10"></textarea>
-          <input type="file" name="file" id="file" />
+          <textarea placeholder="Add Description" rows="10"  style={{
+              borderRadius: "5px",
+              width: "70%",
+              margin: "10px 0 10px 0",
+              padding: "10px",
+              border: "1px #778697 solid",
+            
+            }}></textarea>
+          <input type="file" name="file" id="file" style={{
+              borderRadius: "5px",
+              width: "70%",
+              margin: "10px 0 10px 0",
+              padding: "10px",
+              backgroundColor
+            
+            }}/>
           <button type="submit">Create</button>
         </form>
       </div>
