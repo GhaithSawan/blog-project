a8e569c060dd2d19eb167e6b626f6693941b7bc4
Ghaith <gaesx1x2@gmail.com>
2024-04-23 07:35:51 -0700

    ff

--- frontEnd/requests/postrequest.js
+++ frontEnd/requests/postrequest.js
@@ -2,8 +2,17 @@ import axios from "axios";
 import { Urlaxios } from "../src/constant";
 
 export async function PostRequest(body, path) {
-  let header = {};
-  let data = await axios.post(`${Urlaxios}${path}`, header, body);
-  console.log("data", data);
-//   return data;
+  console.log("body", body);
+  
+  try {
+    let data = await axios.post(`${Urlaxios}${path}`, body,{
+        headers:{
+            authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MjdjNmVkNWZhZTQ5NmZiMTNjMDdjOCIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE3MTM4ODI4NzJ9.XPV5VYC7ATi5FySyskiULpxat-sF7bIlI_jzAe72iGw"
+        }
+    });
+    console.log("data", data);
+  } catch (error) {
+    console.log("PostRequest - error", error);
+  }
+  //   return data;
 }
--- frontEnd/src/pages/CreatePost.jsx
+++ frontEnd/src/pages/CreatePost.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { PostRequest } from "../../requests/postrequest";
 
 const CreatePost = () => {
+  const [post, setPost] = useState({});
 
-  const [addpost, setaddpost] = useState()
-
-  useEffect(() => {
-    PostRequest(
+  useEffect(() => {}, []);
+  const onSub = async (e) => {
+    await PostRequest(
       {
-        title:,
-        description,
-        caticory
-      },"/postRouts/createPost"
-    )
-  }, [])
-  
+        title: e?.target?.input?.value,
+        description: e?.target?.textarea?.value,
+        caticory: e?.target?.select?.value,
+      },
+      "/postRouts/createPost"
+    );
+  };
+
   return (
     <div className="allpages">
       <div
         className="container d-flex align-items-center justify-content-center flex-column p-3"
-        style={{ width: "1000px"}}
+        style={{ width: "1000px" }}
       >
         <h2
           style={{ width: "max-content", fontWeight: "500" }}
@@ -27,7 +29,13 @@ const CreatePost = () => {
           Create New Post
         </h2>
 
-        <form className="container d-flex align-items-center justify-content-center flex-column ">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSub(e);
+          }}
+          className="container d-flex align-items-center justify-content-center flex-column "
+        >
           <input
             type="text"
             placeholder="Add title to your post"
@@ -55,33 +63,44 @@ const CreatePost = () => {
             <option value="Coffee">Coffee</option>
             <option value="music">music</option>
           </select>
-          <textarea placeholder="Add Description" rows="10"  style={{
+          <textarea
+            placeholder="Add Description"
+            rows="10"
+            style={{
               borderRadius: "5px",
               width: "70%",
               margin: "10px 0 10px 0",
               padding: "10px",
               border: "1px #778697 solid",
-            
-            }}></textarea>
-          <input type="file" name="file" id="file" style={{
+            }}
+          ></textarea>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            style={{
               borderRadius: "5px",
               width: "70%",
               margin: "10px 0 10px 0",
               padding: "10px",
-              backgroundColor:"#778697"
-            
-            }}/>
-          <button type="submit" style={{
+              backgroundColor: "#778697",
+            }}
+          />
+          <button
+            type="submit"
+            style={{
               borderRadius: "5px",
               width: "70%",
               margin: "10px 0 10px 0",
               padding: "10px",
               border: "1px #778697 solid",
-              backgroundColor:"#1d2d3d",
-              color:"#fff",
-              fontSize:"20px"
-            
-            }}>Create</button>
+              backgroundColor: "#1d2d3d",
+              color: "#fff",
+              fontSize: "20px",
+            }}
+          >
+            Create
+          </button>
         </form>
       </div>
     </div>
